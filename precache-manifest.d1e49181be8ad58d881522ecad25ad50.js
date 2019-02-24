d7f99accc08510f95df9f9c86bf85e2130d4142a
zzVIPz <viperexe@mail.ru>
2019-02-24 23:52:29 +0300

    add build version

---
+++ precache-manifest.d1e49181be8ad58d881522ecad25ad50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e7b25a2c29706dd3467a",
+    "url": "/static/js/main.e7b25a2c.chunk.js"
+  },
+  {
+    "revision": "6ef6d80f6836e0e8f131",
+    "url": "/static/js/2.6ef6d80f.chunk.js"
+  },
+  {
+    "revision": "e7b25a2c29706dd3467a",
+    "url": "/static/css/main.cd40a11f.chunk.css"
+  },
+  {
+    "revision": "1516e0666a52dc756c7f0752e4d7b417",
+    "url": "/index.html"
+  }
+];
